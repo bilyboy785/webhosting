0120f53600c4a5bffea7025d30b188b5b7ed919d
Martin Bouillaud <contact@bouillaudmartin.fr>
2026-01-07 11:41:39 +0100

    update script with fail2ban config

--- webhosting.sh
+++ webhosting.sh
@@ -571,6 +571,21 @@ pipx install certbot
 pipx inject certbot certbot-dns-cloudflare
 checkreturncode $? "Certbot installation"
 
+subtitle "Configuring Fail2ban"
+if [ -f /etc/fail2ban/jail.local ]; then
+  rm -f /etc/fail2ban/jail.local > /dev/null 2>&1
+fi
+FAIL2BAN_JAIL_SRC="/opt/webhosting/fail2ban/jail.local"
+ln -sf "$FAIL2BAN_JAIL_SRC" /etc/fail2ban/jail.local
+if [ -f /etc/fail2ban/jail.conf ]; then
+  rm -f /etc/fail2ban/jail.conf > /dev/null 2>&1
+fi
+FAIL2BAN_JAIL_SRC="/opt/webhosting/fail2ban/jail.conf"
+ln -sf "$FAIL2BAN_JAIL_SRC" /etc/fail2ban/jail.conf
+systemctl enable fail2ban
+systemctl restart fail2ban
+checkreturncode $? "Fail2ban configuration"
+
 subtitle "Installing WP-CLI"
 if [ ! -f /usr/local/bin/wp ]; then
   curl -s -o /usr/local/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
